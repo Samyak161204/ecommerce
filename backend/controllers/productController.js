c41676893c7b5a6504bc4a0591221e4f9614479c
Samyak161204 <samyakgandhiasia@gmail.com>
2025-04-02 23:35:36 +0530

    fourth

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import productModel from "../models/productmodel.js";
+import productModel from "../models/Productmodel.js";
 
 // Function to add product
 const addProduct = async (req, res) => {
